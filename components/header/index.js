0af2222a06e4dc938a04d2b5fabac9c207b06c12
Jan Gajdos <jangajdos@icloud.com>
2025-03-08 06:05:27 +0100

    Nahrazení SVG loga textovým nápisem STUDIO VISION

--- components/header/index.js
+++ components/header/index.js
@@ -4,103 +4,69 @@ import va from '@vercel/analytics'
 import cn from 'clsx'
 import { ContactForm } from 'components/header/contact-form'
 import { Separator } from 'components/separator'
+import { StudioVisionBanner } from 'components/studio-vision-banner'
 import { pad } from 'lib/maths'
 import { useStore } from 'lib/store'
 import dynamic from 'next/dynamic'
 import s from './header.module.scss'
 
-const VisionLogo = dynamic(() => import('icons/vision-logo.svg'), {
-  ssr: false,
-})
-const VisionLogoMobile = dynamic(() => import('icons/vision-logo-mobile.svg'), {
-  ssr: false,
-})
 const Star = dynamic(() => import('icons/stard.svg'), { ssr: false })
-const VisionMonogram = dynamic(() => import('icons/vision-monogram.svg'), {
-  ssr: false,
-})
-const StarDuotone = dynamic(() => import('icons/star-duotone.svg'), {
-  ssr: false,
-})
 
 export const Header = ({ principles = [], contact }) => {
-  const isMobile = useMediaQuery('(max-width: 800px)')
-
-  // const visible = usePageAppear()
+  const isDesktop = useMediaQuery('(min-width: 800px)')
   const [contactIsOpen, setContactIsOpen] = useStore((state) => [
     state.contactIsOpen,
     state.setContactIsOpen,
   ])
 
   return (
-    <header className={cn(s.container, 'layout-block')}>
-      <div className={cn(s.top, 'layout-grid')}>
-        <div className={s.eggs}>
-          <Link
-            name="easter egg"
-            className={s.egg}
-            href="https://github.com/studio-vision"
-          >
-            <Star />
-          </Link>
-          <Link
-            name="easter egg - soundboard"
-            className={s.egg}
-            href="https://https://vision2-eight.vercel.app/"
-          >
-            <VisionMonogram />
-          </Link>
-          <Link
-            name="easter egg - pale blue dot"
-            className={s.egg}
-            href="https://youtu.be/GO5FwsblpT8"
-          >
-            <StarDuotone />
+    <header className={s.header}>
+      <div className={s.top}>
+        <div className={s.left}>
+          <Link href="/" className={s.logo}>
+            {isDesktop ? (
+              <StudioVisionBanner className={s.title} />
+            ) : (
+              <StudioVisionBanner className={s.title} />
+            )}
           </Link>
         </div>
-        {isMobile === false && (
-          <Marquee className={s.marquee} duration={20}>
-            {principles.map((principle, i) => (
-              <p key={i} className={cn('p', s.principle)}>
-                <span>{pad(i + 1)}</span>
-                &nbsp;{principle}
-                <span className={s.separator}>{'//'}</span>
-              </p>
-            ))}
-          </Marquee>
-        )}
-        <button
-          className={cn('button', s.cta)}
-          onClick={() => {
-            va.track('Opened Contact Form')
-            setContactIsOpen(!contactIsOpen)
-          }}
-        >
-          Contact
-        </button>
-      </div>
-      <Separator />
-      <div className={cn(s.header, 'layout-grid')}>
-        {isMobile === true ? (
-          <VisionLogoMobile className={s.title} />
-        ) : (
-          <VisionLogo className={s.title} />
-        )}
-      </div>
-      <Separator />
 
-      {isMobile === true && (
-        <Marquee className={s.marquee} duration={20}>
-          {principles.map((principle, i) => (
-            <p key={i} className={cn('p', s.principle)}>
-              <span>{pad(i + 1)}</span>
-              &nbsp;{principle}
-              <span className={s.separator}>{'//'}</span>
-            </p>
-          ))}
+        <div className={s.right}>
+          <div className={s.principles}>
+            {principles.map((principle, i) => (
+              <div className={s.principle} key={i}>
+                <p className="p-xs text-uppercase">
+                  {pad(i + 1, 2)} {principle}
+                </p>
+                {i < principles.length - 1 && <Star className={s.star} />}
+              </div>
+            ))}
+          </div>
+          <button
+            className={cn('p-s decorate', s.contact)}
+            onClick={() => {
+              va.track('Opened Contact Form')
+              setContactIsOpen(!contactIsOpen)
+            }}
+          >
+            {contactIsOpen ? 'close' : 'contact'}
+          </button>
+        </div>
+      </div>
+      <Separator />
+      <div className={cn(s.marquee, contactIsOpen && s.hidden)}>
+        <Marquee speed={isDesktop ? 1.5 : 0.5}>
+          <p className="p-xs text-uppercase">
+            Studio Vision is an independent creative studio crafting brands,
+            digital experiences, and solutions that push missions to new
+            heights.
+          </p>
         </Marquee>
-      )}
-      <ContactForm data={contact} />
+      </div>
+      <div className={cn(s.form, !contactIsOpen && s.hidden)}>
+        <ContactForm data={contact} />
+      </div>
     </header>
   )
 }
